69d8873a60d8b420aadfec7bd9e15a4d9b479fae
D22NK <daankentrop@gmail.com>
2021-04-05 11:15:01 +0200

    rm uploads and db code

--- api/main.py
+++ api/main.py
@@ -5,22 +5,13 @@ from fastapi.responses import HTMLResponse
 import uuid
 
 from fastapi.middleware.cors import CORSMiddleware
-
-from pymongo import MongoClient
-import datetime
-# from tester import predict
-
-
 from PIL import Image
 import numpy as np
 import os, cv2
 import tensorflow as tf
 
 app = FastAPI()
-# client = MongoClient(
-#     'mongodb+srv://AIGANG:Ez6DFjF8XJttNMLF@dk-1.sflh5.mongodb.net/AIGANG?retryWrites=true&w=majority')
-# db = client.AIGANG
-# collection = db.AIGANG
+
 
 origins = [
     "http://localhost:8000",
@@ -45,37 +36,14 @@ async def create_upload_files(files: List[UploadFile] = File(...)):
             shutil.copyfileobj(image.file, buffer)
         shutil.move("uploads/" + image.filename,
                     "uploads/" + new_name + ".jpg")
-        item = {
-            "id": new_name,
-            "date": datetime.datetime.utcnow(),
-            "age": 0,
-            "status": "Uploaded"
-        }
-        # collection.insert_one(item)
-        print('saved')
         filename = new_name + ".jpg"
         filelocation = "./uploads/" + filename
         os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'
-        print('load model')
         model = tf.keras.models.load_model("./")
-        print('read image')
         test_pic = cv2.imread(filelocation)
-        print('resize')
         test_pic = cv2.resize(test_pic,(128,128))
-        print('reshape')
         test_pic = test_pic.reshape((1,128,128,3))
-        print('predict')
         pred = model.predict(test_pic)
-        print('result')
         age = pred.tolist()
     return  {"UploadedFileName": filename, "age": round(age[0][0], 0)}
 
-
-# @app.get("/info/{id}")
-# def getInfo(id):
-#     return collection.find_one({"id": id}, {'_id': 0})
-
-
-@app.get("/")
-def home():
-    return {"Ageprediction API": "Welcome"}
