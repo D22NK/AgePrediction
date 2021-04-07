c7bd3569a2296da260167e3a447186f01efdf22d
D22NK <daankentrop@gmail.com>
2021-04-07 10:16:01 +0200

    Update

--- api/tester.py
+++ api/tester.py
@@ -3,11 +3,11 @@ import numpy as np
 import os, cv2
 import tensorflow as tf
 
-async def predict(filelocation):
-    os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'
-    model = tf.keras.models.load_model("./")
-    test_pic = cv2.imread(filelocation)
-    test_pic = cv2.resize(test_pic,(128,128))
-    test_pic = test_pic.reshape((1,128,128,3))
-    pred = model.predict(test_pic)
-    return pred
+
+os.environ['TF_XLA_FLAGS'] = '--tf_xla_enable_xla_devices'
+model = tf.keras.models.load_model("./")
+test_pic = cv2.imread('../../victor.png')
+test_pic = cv2.resize(test_pic,(128,128))
+test_pic = test_pic.reshape((1,128,128,3))
+pred = model.predict(test_pic)
+print(pred)
